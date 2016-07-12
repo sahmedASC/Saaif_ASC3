1b673162a943c359498096c3c3d93a8b2b67be3f
sahmedASC <saaifahmed12@gmail.com>
2016-07-12 15:01:36 -0400

    This is our (Jason and Saaif) work.

---
+++ robot dance.py
@@ -0,0 +1,19 @@
+#Made by Saaif Ahmed and Jason Zhagui
+#Song used- Spider Dance (Undertale) by Toby Fox
+#We plan to create the loops once we have the movements down for the parts
+from Myro import *
+
+#verse
+wait(4.4)
+turnRight(1,.1)
+turnLeft(1,.2)
+turnRight(1,.2)
+turnLeft(1,.1)
+forward(1,.4)
+backward(1,.4)
+forward(1,.4)
+backward(1,.4)
+turnBy(180)
+#chorus
+
+#bridge
